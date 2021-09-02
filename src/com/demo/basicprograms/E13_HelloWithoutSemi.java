6f737a8caa20c48acab6777688f3b6fabb4a31d4
Sumit Paswan <sumitpaswan942@gmail.com>
2021-09-02 23:18:22 +0530

    Printing Hello world without using Semicolon

---
+++ src/com/demo/basicprograms/E13_HelloWithoutSemi.java
@@ -0,0 +1,14 @@
+package com.demo.basicprograms;
+
+public class E13_HelloWithoutSemi {
+
+	public static void main(String[] args) {
+		
+		/****** Method 1 ******/
+		if (System.out.printf("Hello World")!=null) {}
+
+		/****** Method 2 ******/
+//		for(System.out.printf("Hello World 2");;) {}
+	}
+
+}
